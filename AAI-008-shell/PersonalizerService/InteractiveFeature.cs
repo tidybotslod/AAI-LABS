d34744ee65ca7ff3378764e16b991a7c3acea565
tidybots <tidybotslod@outlook.com>
2021-04-21 12:32:29 -0700

    Coped last changes for 008 to the 008 shell directory.

--- AAI-008-shell/PersonalizerService/InteractiveFeature.cs
+++ AAI-008-shell/PersonalizerService/InteractiveFeature.cs
@@ -20,16 +20,16 @@ namespace AAI
             }
         }
 
-        // Private methods, not publicly accessable.
+        // Private methods, not publicly accessible.
         static private string BuildPrompt(string prompt, string[] entries)
         {
             StringBuilder ask = new StringBuilder($"{prompt} (enter number or Q to quit)?", 256);
             if (entries.Length > 0)
             {
-                ask.Append($" 1. {entries[0]}");
+                ask.Append($"\n1. {entries[0]}");
                 for (int i = 1; i < entries.Length; i++)
                 {
-                    ask.Append($", {i + 1}. {entries[i]}");
+                    ask.Append($"\n{i + 1}. {entries[i]}");
                 }
             }
             return ask.ToString();
